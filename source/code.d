160ca72d31c92cd6c8e6dff071a89f78b8ddd987
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2025-07-13 18:50:15 -0600

    Adjust code snippets to latest dlango change.

--- source/code.d
+++ source/code.d
@@ -14,9 +14,10 @@ const auto code2 = q{struct User {
 const auto code3 = q{// new user insertion
 User("retrac", "retrac@gmail.com", "password").save();
 // filter by email, case insensitive
-auto filtered = User.objects.filter([
-    Q("email__iexact", "RETRAC@gmail.com")
-]);
+auto filtered = User.objects.filter(
+    Q("email__iexact", "RETRAC@gmail.com"),
+    Q("username__contains", "trac")
+);
 // print results
 foreach (user; filtered) {
     writeln(user.username, " ", user.email, " ", user.ID);
